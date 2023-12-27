7a799866d7d481dd5d5bb18374ad05357ab040fa
Hassam Ullah <malikhassamullah1@yahoo.com>
2023-12-27 10:35:31 +0500

    first commit

---
+++ lib/elixir_ecto.ex
@@ -0,0 +1,18 @@
+defmodule ElixirEcto do
+  @moduledoc """
+  Documentation for `ElixirEcto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirEcto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_ecto_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirEctoTest do
+  use ExUnit.Case
+  doctest ElixirEcto
+
+  test "greets the world" do
+    assert ElixirEcto.hello() == :world
+  end
+end
